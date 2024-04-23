7b05505e010b23f55133b7bae8ec85549e10ed55
rhinoandre <rhinoandre@gmail.com>
2024-04-23 15:14:18 -0300

    chore: clean up

--- src/use-cases/factories/make-example-use-case.ts
+++
@@ -1,9 +0,0 @@
-// import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-// import { RegisterUseCase } from '../register'
-
-// export function makeRegisterUseCase() {
-//   const usersRepository = new PrismaUsersRepository()
-//   const useCase = new RegisterUseCase(usersRepository)
-
-//   return useCase
-// }
