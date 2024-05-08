3ca1765fda02f7ceb778e8400d2a0843726dcc29
rhinoandre <rhinoandre@gmail.com>
2024-05-08 09:09:11 -0300

    feat: simulate growth estimative

--- src/http/controller/calculator.ts
+++ src/http/controller/calculator.ts
@@ -4,16 +4,20 @@ import { z } from "zod";
 import serviceCalculators from "../../services";
 const enuns = Object.keys(serviceCalculators);
 
-const configSchema = z.array(
-  z.object({
-    name: z.enum(enuns as [string, ...string[]]),
-    fields: z.array(z.any()),
-  })
-);
+const configSchema = z.object({
+  services: z.array(
+    z.object({
+      name: z.enum(enuns as [string, ...string[]]),
+      fields: z.array(z.any()),
+    }),
+  ),
+  growthFactor: z.number().optional(),
+});
 
 interface ServiceCost {
   services: any[];
   totalCost: 0;
+  totalGrowthCost: 0;
 }
 
 export async function calculator(
@@ -31,16 +35,30 @@ export async function calculator(
   const result: ServiceCost = {
     services: [],
     totalCost: 0,
+    totalGrowthCost: 0,
   };
-
-  for (const service of body.data) {
+  const { services, growthFactor } = body.data;
+  for (const service of services) {
     const calculator = serviceCalculators[service.name];
     const serviceCost = await calculator.calculate(service.fields);
+    let growthCost = 0;
+    if (growthFactor && growthFactor !== 1) {
+      growthCost = await calculator.calculate(service.fields.map((field) => {
+        const value = Number(field.value) ? field.value * growthFactor : field.value;
+        return {
+          ...field,
+          value
+        }
+      }));
+    }
+
     result.services.push({
       name: service.name,
       cost: serviceCost,
+      growthCost,
     });
     result.totalCost += serviceCost;
+    result.totalGrowthCost += growthCost;
   }
 
   return result;
